dd20577f016d81e40159c432a8c483627d8b022e
Mike Sun <mightnent@gmail.com>
2019-07-26 08:01:01 -0400

    upload main.py

---
+++ main.py
@@ -0,0 +1,116 @@
+
+
+# Import packages
+import os
+import cv2
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import tensorflow as tf
+import argparse
+import sys
+from utils import movement as mv
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+IM_WIDTH = 640    
+IM_HEIGHT = 480   
+
+# Select camera type (if user enters --usbcam when calling this script,
+# a USB webcam will be used)
+camera_type = 'picamera'
+parser = argparse.ArgumentParser()
+parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
+                    action='store_true')
+args = parser.parse_args()
+if args.usbcam:
+    camera_type = 'usb'
+
+
+sys.path.append('..')
+print(sys.path)
+
+MODEL_NAME = 'myModel'
+
+# working dr
+CWD_PATH = os.getcwd()
+
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+
+PATH_TO_LABELS = os.path.join(CWD_PATH,'data','labelmap.pbtxt')
+
+# Number of classes the object detector can identify
+NUM_CLASSES = 3
+
+## Load the label map so that we know what the index corresponds to
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+#tensorflow model load
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.compat.v1.Session(graph=detection_graph)
+
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+
+# show score
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+
+# Number of objects detected, purely for my own reference. This kinda handy if not headless like this pi
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+
+# calc fps
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+### USB webcam ###
+if camera_type == 'usb':
+    # Initialize USB webcam feed
+    camera = cv2.VideoCapture(0)
+    ret = camera.set(3,IM_WIDTH)
+    ret = camera.set(4,IM_HEIGHT)
+
+    while(True):
+
+        # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+        # i.e. a single-column array, where each item in the column has the pixel RGB value
+        ret, frame = camera.read()
+        frame_expanded = np.expand_dims(frame, axis=0)
+
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
+
+        # visualize and move
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.67)
+        
+
+cv2.destroyAllWindows()
+
